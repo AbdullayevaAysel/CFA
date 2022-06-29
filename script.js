a9d8a8cd2c6418ea0ff0c618f3c5420dd9f61067
Aysel Abdullayeva <abdulla2001aysel@gmail.com>
2022-06-30 01:34:09 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector(".navbar-nav");
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+    if (nav.classList.contains("show")) {
+        nav.classList.remove("show");
+    } else {
+        nav.classList.add("show");
+    }
+});
+
+// chatbot
+const wrapper = document.querySelector(".chatbot .wrapper");
+document.querySelector(".show-btn").addEventListener("click", () => {
+    if (wrapper.classList.contains("show-chatbot")) {
+        wrapper.classList.remove("show-chatbot");
+    } else {
+        wrapper.classList.add("show-chatbot");
+    }
+});
+
+document.querySelector(".chatbot .next").addEventListener("click", () => {
+    wrapper.classList.remove("show-chatbot");
+});
